099ea21af3fbd1b8a5cbef5796a299e9c5b9a7de
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-09-21 13:00:34 -0400

    added environmental variables

--- weather-guardian/src/App.js
+++ weather-guardian/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
+const URL =
+  'https://f447a5d5-06b6-4489-9695-04ef87e544f4:HG44e1OJo4@twcservice.mybluemix.net/api/weather/v1/geocode/33.40/-83.42/observations.json';
+
+async function getWeather() {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: URL,
+      params: {
+        language: 'en-US'
+      }
+    });
+
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 export default function App() {
+  getWeather();
   return (
     <div className="App">
       <h1>Weather guardian unleashed!</h1>
--- weather-guardian/src/index.js
+++ weather-guardian/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { config } from 'dotenv';
+
+config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
