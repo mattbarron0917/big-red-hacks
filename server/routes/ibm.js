c266713bd6739a4d5519aadec3a3d426457742dd
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-09-21 16:04:22 -0400

    added more to route link

--- server/routes/ibm.js
+++ server/routes/ibm.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getForcast, getLocationThroughZipCode } = require('../ibmApi/ibmUtility');
 const router = express.Router();
 
-router.get('/:zipcode', async function(req, res, next) {
+router.get('/forcast/:zipcode', async function(req, res, next) {
   try {
     const { latitude, longitude } = await getLocationThroughZipCode(req.params.zipcode);
     const forcast = await getForcast(latitude, longitude);
